052909ffb6770aa081b6f4892b62ce5681c0739e
evan7892 <evanalvarez789@gmail.com>
2022-08-17 19:11:28 -0500

    agregué librerias a mi proyecto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,151 @@
+const btnSwal = document.getElementById("btnUno")
+const btnSwal2 = document.getElementById("btnDos")
+const btnSwal3 = document.getElementById("btnTres")
+const btnSwal4 = document.getElementById("btnCuatro")
+const btnSwal5 = document.getElementById("btnCinco")
+const btnSwal6 = document.getElementById("btnSeis")
+
+btnSwal.onclick=()=>{
+    Swal.fire({
+        title: '¿Deseas añadir Microfono Hyper x Quadcast al carrito?',
+        text: "Se añadirá a tu carrito",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, deseo añadirlo',
+        cancelButtonText:"No deseo añadirlo"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Añadiste Hyper x Quadcast al carrito ',
+            'Tu producto se añadió al carrito.',
+            
+          )
+        }
+      })
+}
+btnSwal2.onclick=()=>{
+    Swal.fire({
+        title: '¿Deseas añadir Mouse Logitech G203 al carrito?',
+        text: "Se añadirá a tu carrito",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, deseo añadirlo',
+        cancelButtonText:"No deseo añadirlo"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Añadiste Mouse Logitech G203 al carrito ',
+            'Tu producto se añadió al carrito.',
+            
+          )
+        }
+      })
+}
+btnSwal3.onclick=()=>{
+    Swal.fire({
+        title: '¿Deseas añadir Logitech brio 4k al carrito?',
+        text: "Se añadirá a tu carrito",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, deseo añadirlo',
+        cancelButtonText:"No deseo añadirlo"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Añadiste Logitech brio 4k al carrito ',
+            'Tu producto se añadió al carrito.',
+            
+          )
+        }
+      })
+}
+btnSwal4.onclick=()=>{
+    Swal.fire({
+        title: '¿Deseas añadir Monitor gamer Samsung F22T35 al carrito?',
+        text: "Se añadirá a tu carrito",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, deseo añadirlo',
+        cancelButtonText:"No deseo añadirlo"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Añadiste Monitor gamer Samsung F22T35 al carrito ',
+            'Tu producto se añadió al carrito.',
+            
+          )
+        }
+      })
+}
+btnSwal5.onclick=()=>{
+    Swal.fire({
+        title: '¿Deseas añadir Teclado mecánico logitech G413 al carrito?',
+        text: "Se añadirá a tu carrito",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, deseo añadirlo',
+        cancelButtonText:"No deseo añadirlo"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Añadiste Teclado mecánico logitech G413 al carrito ',
+            'Tu producto se añadió al carrito.',
+            
+          )
+        }
+      })
+}
+btnSwal6.onclick=()=>{
+    Swal.fire({
+        title: '¿Deseas añadir Mouse Pad gamer Logitech G6403 al carrito?',
+        text: "Se añadirá a tu carrito",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, deseo añadirlo',
+        cancelButtonText:"No deseo añadirlo"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Añadiste Mouse Pad gamer Logitech G640 al carrito ',
+            'Tu producto se añadió al carrito.',
+            
+          )
+        }
+      })
+}
+
+
+let readMore_btn = document.getElementById('readMore_btn');
+let hideText = document.getElementById('foot');
+
+readMore_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+    hideText.classList.toggle('show');
+
+    if (hideText.classList.contains('show')) {
+        readMore_btn.innerHTML = 'Ver menos'
+    }
+    else {
+        readMore_btn.innerHTML = 'Ver más'
+    }
+}
+
+
+
+
 let loginSubmit = document.getElementById("loginSubmit");
 let olvidar = document.getElementById("olvidar");
 let borrarTodo = document.getElementById("borrarTodo");
@@ -47,26 +195,7 @@ function recordarme(storage) {
 
     borrarTodo.addEventListener("click",()=>{
         borrarDatos(sessionStorage);
-        borrarDatos(localStorage)
+        borrarDatos(localStorage);
     }) 
 
 
-
-    const btnComprar = document.getElementById("botonComprar")
-    btnComprar.addEventListener("click", () => alert("Aún no está disponible"));
-
-    let readMore_btn = document.getElementById('readMore_btn');
-    let hideText = document.getElementById('foot');
-
-    readMore_btn.addEventListener('click', toggleText);
-
-    function toggleText() {
-        hideText.classList.toggle('show');
-
-        if (hideText.classList.contains('show')) {
-            readMore_btn.innerHTML = 'Ver menos'
-        }
-        else {
-            readMore_btn.innerHTML = 'Ver más'
-        }
-    }
